bd21f9bb2f99f3ac1771ce63f55e742df629d056
Vinícius Jesus <vinicius.je99@gmail.com>
2021-07-14 22:09:22 -0300

    fix github pages links

--- scripts/signIn.js
+++ scripts/signIn.js
@@ -8,7 +8,7 @@ function signIn(){
     auth.signInWithEmailAndPassword(email, pwd)
         .then(user => {
             console.log("Usuário conectado")
-            window.location.href = "https://vinicius-je.github.io/todo-list-firebase/todo.html";
+            window.location.href = "https://vinicius-je.github.io/todo-list-firebase/todo";
         })
         .catch(err => { alert("missing email or password") })
 }
